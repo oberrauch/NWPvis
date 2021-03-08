2ee4bc357289f0ae99f510ea019cb0b79c1ea6a0
oberrauch <oberrauch.moritz@gmail.com>
2021-03-08 11:43:27 +0100

    Documentation housekeeping

--- constants.py
+++ constants.py
@@ -1,35 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+""" Physical constants
 
-##############################################################################
-#
-# Author : Alzbeta Medvedova
-#
-# This module contains physical constant necessary for calculations which are
-# needed to derive physical quantities not contained in the original dataset.
-#
-##############################################################################
+This module contains physical constant necessary for calculations of quantities
+not contained in the original dataset.
+
+Author(s): Alzbeta Medvedova, Moritz Oberrauch
+
+References:
+
+    American Meteorological Society (AMS), 2018: Glossary of Meteorology:
+    geopotential height, https://glossary.ametsoc.org/wiki/Geopotential_height
+
+    Markowski P. and Richardson Y., 2016: Mesoscale Meteorology in Midlatitudes
+    Wiley-Blackwell, Royal Meteorological Society, ISBN: 978-0-470-74213-6
+
+    Stull, R., 2011: Meteorology for Scientists & Engineers, 3rd Edition
+    Univ. of British Columbia.  938 pages.  ISBN 978-0-88865-178-5
+
+"""
+
+# Gravitational acceleration, global average at sea level (AMS 2018)
+g = 9.80665  # [m/s^2]
 
 
-
-# Gravity taken from: http://glossary.ametsoc.org/wiki/Geopotential_height
-g = 9.80665     # [m/s^2] globally averaged gravity at sea leve
-
-
-# Gas constants for the equation of state are taken from:
-# P. Markowski and Y. Richardson, 2016: Mesoscale Meteorology in Midlatitudes
-# Wiley-Blackwell, Royal Meteorological Society, ISBN: 978-0-470-74213-6
+# Gas constants for the equation of state (Markowski and Richardson, 2016)
 Rd = 287.04     # [J/kg/K] gas constant, dry air
 Rvap = 461.51   # [J/kg/K] gas constant, water vapor
 
-t_0 = 273.15    # [K] temperature
-p0 = 1e5        # [Pa] reference pressure (100 hPa)
-c_p = 1005.7    # [J/kg/K] specific heat of dry air at consant pressure
-c_l = 4190      # [J/kg/K] specific heat of liquid water at consant pressure
+# Specific heat capacities
+# TODO: REF and exact value for specific heat of liquid water
+c_p = 1005.7    # [J/kg/K] specific heat of dry air at constant pressure
+c_l = 4190      # [J/kg/K] specific heat of liquid water at constant pressure
 rcp = 0.2854    # [-] Rd/c_p
 
-# TODO: REF and exact value for specific heat of liquid water
+# reference values
+t_0 = 273.15    # [K] reference temperature (0 degC)
+p0 = 1e5        # [Pa] reference pressure (100 hPa)
 
-
-# Stull, R., 2011: "Meteorology for Scientists & Engineers, 3rd Edition
+# Compute dry adiabatic lapse rate following Stull (2011)
 gamma_d = g/c_p  # [K/m] dry adiabatic lapse rate
